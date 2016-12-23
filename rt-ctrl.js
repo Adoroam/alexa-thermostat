5ac40000fce58e8b092f487016b5f25a784e72a6
Anthony Stabile <astabile.design@gmail.com>
2016-12-22 22:35:47 -0600

    now i get a success but no visible change

--- rt-ctrl.js
+++ rt-ctrl.js
@@ -1,18 +1,26 @@
-const http = require('http');
+//do something with the temp
+const radio1 = require('./radio1');
+const radio2 = require('./radio2');
+
+
 
 module.exports = {
 	test: function(arg) {
-		if (arg) console.log(`ran test with ${arg}`);
-		else console.log('ran test without arg');
+		//
 	},
-	setTemp(targetTemp) {
-		//post to set temp
+	tempHeat: function(thermostat, target) {
+		var postObj = { t_heat: target };
+		if (thermostat == '192.168.2.215') radio1.post(postObj);
+		if (thermostat == '192.168.2.223') radio2.post(postObj);
+
+	},
+	setTemp(thermostat, targetTemp) {
+		//post request to thermostat
+
 	},
 	raiseTemp(increase) {
 		//get temp
 		//add increase
 		//set temp
-	},
-	options(method, ip ) {
 	}
 };
--- start.js
+++ start.js
@@ -1,8 +1,69 @@
-const rt = require('./rt-ctrl');
+//const rt = require('./rt-ctrl');
+const http = require('http');
+const querystring = require('querystring');
+const thermostat = {
+	up: '192.168.2.215',
+	down: '192.168.2.223'
+};
 
-var downstairs = '99.149.178.119';
-var upstairs = 'other ip';
+//rt.tempHeat(thermostat.up, tempTest);
+function thermostatSend(thermostat, postObj){
+	var post_data = querystring.stringify(postObj, ',', ':');
+	var postOptions = {
+		host: thermostat,
+		path: '/tstat',
+		method: 'POST',
+		headers: {
+	        'Content-Type': 'application/x-www-form-urlencoded',
+        	'Content-Length': Buffer.byteLength(post_data)
+      	}
+	};	
+	var post_req = http.request(postOptions, function(res) {
+		res.setEncoding('utf8');
+		res.on('data', function (chunk) {
+			console.log('Response: ' + chunk);
+		});
+	});
+
+	// post the data
+	post_req.write(post_data);
+	post_req.end();
+
+};
+
+function tempHeat(thermostat, target) {
+	var postObj = { t_heat: target };
+	thermostatSend(thermostat, postObj);
+}
+
+
+tempHeat(thermostat.up, 65);
+
+/*
 var selected = null;
+
+function urlString(thermostat) {
+	return 'http://'+thermostat+'/tstat';
+};
+function getData(data){
+	console.log(data);
+};
+var req = http.get(urlString(upstairs), (res) => {
+	res.setEncoding('utf8');
+	let rawData = '';
+	res.on('data', (chunk) => {
+		rawData += chunk;
+	});
+	res.on('end', () => {
+		try {
+			let parsedData = JSON.parse(rawData);
+		 	getData(parsedData);
+		} catch (e) {
+			console.log(e.message);
+		}
+	});
+});*/
+
 /*
 	alexa ac
 	-which ac would you like to change?
@@ -17,4 +78,24 @@ var selected = null;
 		rt.setTemp(78)
 
 */
-console.log("ran startup script");
+//console.log(rt.getTemp(upstairs));
+//rt.getTemp(upstairs);
+
+//get thermostat info
+//if (given.)
+
+/*
+object template
+{
+	temp: 71,
+	tmode: 1,
+	fmode: 0,
+	override: 1,
+	hold: 1,
+	t_heat: 59,
+	tstate: 0,
+	fstate: 0,
+	time: { day: 3, hour: 20, minute: 58 },
+	t_type_post: 0
+}
+*/
